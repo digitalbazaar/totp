07dce8bdf3a4c652240fde3e30868e726a440b97
Dave Longley <dlongley@digitalbazaar.com>
2022-05-22 13:22:54 -0400

    Add initial files.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,4 @@
-# isomorphic-js-lib-template Changelog
+# totp Changelog
 
 ## 1.0.0 - TBD
 
---
+++ tests/benchmark.js
@@ -0,0 +1,28 @@
+/*!
+* Copyright (c) 2020-2022 Digital Bazaar, Inc. All rights reserved.
+*/
+import Benchmark from 'benchmark';
+
+const suite = new Benchmark.Suite();
+
+import {
+  generateSecret, generateToken, verify
+} from '../lib/index.js';
+
+suite
+  .add('generateToken', {
+    defer: true,
+    fn: async deferred => {
+      // FIXME:
+      const params = await generateSecret();
+      await generateToken(params);
+      deferred.resolve();
+    }
+  })
+  .on('cycle', event => {
+    console.log(String(event.target));
+  })
+  .on('complete', function() {
+    console.log('Fastest is ' + this.filter('fastest').map('name'));
+  })
+  .run();
